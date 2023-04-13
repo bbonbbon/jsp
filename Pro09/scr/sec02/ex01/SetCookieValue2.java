d89ba017976d23a7d447408b71c9d786c40160fa
bbonbbon <127382033+bbonbbon@users.noreply.github.com>
2023-04-13 17:00:21 +0900

    23.04.13

---
+++ Pro09/scr/sec02/ex01/SetCookieValue2.java
@@ -0,0 +1,36 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+
+import java.util.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SetCookieValue
+ */
+@WebServlet("/set2")
+public class SetCookieValue2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		Date d=new Date();
+		
+		Cookie c =new Cookie("cookieTest",URLEncoder.encode("JSP프로그램 입니다.","utf-8"));
+		
+		c.setMaxAge(-1);
+		response.addCookie(c);
+		out.println("현재 시간 :" +d);
+		out.println("문자열을 cookie에 저장합니다.");
+	}
+
+}
